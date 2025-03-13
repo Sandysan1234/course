cb72702cf697304a9c877a1e9ed7b1d91a60b519
sandysan1234 <rahmahdiandian@gmail.com>
2025-03-13 20:08:36 +0700

    memperbarui dicoding

--- dicoding/index.js
+++ dicoding/index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/mahasiswa/:name", (req, res) => {
+  res.send(`halo ${req.params.name}, category= ${req.query.cat} `)
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log('Server is running on http://localhost:3000');
+});
+ 
