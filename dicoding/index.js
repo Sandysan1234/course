e81d5b0103694ab5b72fe030e1d3e64b60c8116b
sandysan1234 <231080200141@umsida.ac.id>
2025-03-20 16:29:49 +0700

    coba testing api & belajar routing api

--- dicoding/index.js
+++ dicoding/index.js
@@ -5,8 +5,19 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.get("/mahasiswa/:name", (req, res) => {
-  res.send(`halo ${req.params.name}, category= ${req.query.cat} `)
+  res.send(`halo ${req.params.name}, category = ${req.query.c} `)
+});
+
+app.get("/user/:name?", (req, res) => {
+  const name = req.params.name || 'stranger'
+  res.send(`hello ${name}`)
+  // res.send(`halo ${req.params.name}`)
+});
+
+app.get("/about", (req, res) => {
+  res.send(`About Page`)
 });
 
 app.listen(port, () => {
